517ed555729e64fc2a9936b9033b2513ebe04f10
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2025-09-23 20:54:36 +0530

    Added code for the server

--- src/socket.c
+++ src/socket.c
@@ -1,5 +1,20 @@
 #include <sys/socket.h>
 
+
+static void do_something(int conn_fd) {
+    char rbuf[64] = {};
+
+    ssize_t n = read (conn_fd, rbuf, sizeof(rbuf) - 1); // can be replaced with recv(), requires additional flags
+    if (n < 0) {
+        msg("read() error");
+        return;
+    }
+    printf("Client says: %s\n", rbuf);
+
+    char wbuf[] = "world"; // sizeof also includes the null terminator
+    write(conn_fd, wbuf, strlen(wbuf)); // can be replaced with send(), requires additional flags
+}
+
 int fd = socket(AF_INET, SOCK_STREAM, 0);
 
 int val = 1;
@@ -10,11 +25,24 @@ addr.sin_family = AF_INET;
 addr.sin_port = htons(8080);  //port
 addr.sin_addr.s_addr = htonl(0) //wildcard ip 0.0.0.0    INADDR_ANY; // listen to all addresses
 
+// bind
 int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
-if (rv) { die("bind()"); }
+if (rv) { die("bind()"); } 
 
-listen(fd)
-while True:
-    conn_fd = accept(fd)
-    do_something(conn_fd)
-    close(conn_fd)
+// listen
+rv = listen(fd, SOMAXCONN);
+if (rv) {die("listen()");} 
+
+while (true) {
+    // accept
+    struct sockaddr_in client_addr = {};
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    // Here we pass the struct of sockaddr for client address, along with the length of the struct
+    int client_fd = accept(fd, (struct sockaddr *)&client_addr, &client_addr_len); 
+    if (client_fd < 0) {
+        continue; //error handling
+    }
+    do_something(client_fd); //handle the client connection
+    close(client_fd);
+}
