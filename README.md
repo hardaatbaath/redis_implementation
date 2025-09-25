bc592941b1d783ae62e170c4fa4e9adf3bd16389
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2025-09-25 14:52:22 +0530

    Added client and make file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # redis_implementation
 This is my implementation of basic functionality of redis in C
+
+
+How to use
+Build everything: make
+Run server: make run-server
+Run client (in another terminal): make run-client
+Clean/rebuild: make rebuild
---
+++ src/utils.h
@@ -0,0 +1,6 @@
+// src/utils.h
+#pragma once // to prevent multiple inclusion, as this header file is included in multiple files
+#include <stddef.h> 
+
+void msg(const char *msg);
+[[noreturn]] void die(const char *msg); // to prevent the function from returning
