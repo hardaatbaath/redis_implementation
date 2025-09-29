e8a5ee24b4a6c78a8d8d4645b7d00caeafa39b67
Hardaat Singh Baath <101502407+hardaatbaath@users.noreply.github.com>
2025-09-30 00:54:49 +0530

    Updated and bug fixes

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,25 +1,31 @@
 // src/utils.cpp
 #include "utils.h"
 
-#include <assert.h> 
+// C stdlib
+#include <assert.h>
 #include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> // for read, write
-#include <fcntl.h> // for fcntl
+
+// POSIX / system
+#include <unistd.h> // read, write
+#include <fcntl.h>  // fcntl
+
+// C++ stdlib
+#include <vector>
 
 /**
  * Print a message to stderr
  */
-[[noreturn]] void msg(const char* message) {
+void msg(const char* message) {
     fprintf(stderr, "%s\n", message);
 }
 
 /**
  * Print an error message to stderr
  */
-[[noreturn]] void msg_error(const char* message) {
+void msg_error(const char* message) {
     fprintf(stderr, "[ERROR] %s\n", message);
 }
 
@@ -35,7 +41,7 @@
 /**
  * Set the file descriptor to non-blocking mode
  */
-[[noreturn]] void fd_set_nb(int fd) {
+void fd_set_nb(int fd) {
     errno = 0;
     int flags = fcntl(fd, F_GETFL, 0);
     if (errno) { die("fcntl() read error"); }
@@ -79,4 +85,14 @@ int32_t write_all(int fd, char* buf, size_t n){
         buf += rv;
     }
     return 0;
+}
+
+// Buffer utilities
+void append_buffer(std::vector<uint8_t>& buffer, const uint8_t* data, size_t len) {
+    buffer.insert(buffer.end(), data, data + len);
+}
+
+void consume_buffer(std::vector<uint8_t>& buffer, size_t len) {
+    if (len > buffer.size()) { len = buffer.size(); }
+    buffer.erase(buffer.begin(), buffer.begin() + len);
 }
