bcb5f75e65aa97d46274b053fbf01adf2f5fcdd8
Hardaat Singh Baath <101502407+hardaatbaath@users.noreply.github.com>
2025-10-19 11:07:31 +0530

    Added basic support for hash maps and rehashing

--- src/constants.h
+++ src/constants.h
@@ -7,3 +7,9 @@ const size_t k_max_msg = 32u << 20; // 32 MiB
 
 // Maximum number of arguments for a command
 const size_t k_max_args = 200 * 1000; // 200,000 arguments
+
+// Maximum load factor for the hash table
+const size_t k_max_load_factor = 8;
+
+// Constant workload for rehashing
+const size_t k_rehashing_work = 128;
--- src/hashtable.h
+++ src/hashtable.h
@@ -13,18 +13,19 @@ struct HashTable {
 };
 
 struct HashMap {
-    HashTable *older;
-    HashTable *newer;
+    HashTable older;
+    HashTable newer;
     size_t migration_pos = 0;    // the position of the migration in the newer table
 };
 
 // HashTable functions
 void h_init(HashTable *ht, size_t size);
-void h_insert(HashTable *ht, HashNode *node);
 HashNode **h_lookup(HashTable *ht, HashNode *key, bool (*eq)(HashNode *, HashNode *));
+void h_insert(HashTable *ht, HashNode *node);
 HashNode *h_detach(HashTable *ht, HashNode **node);
 
 // HashMap functions
+void   hm_trigger_rehash(HashMap *hmap);
 HashNode *hm_lookup(HashMap *hmap, HashNode *key, bool (*eq)(HashNode *, HashNode *));
 void   hm_insert(HashMap *hmap, HashNode *node);
 HashNode *hm_delete(HashMap *hmap, HashNode *key, bool (*eq)(HashNode *, HashNode *));
